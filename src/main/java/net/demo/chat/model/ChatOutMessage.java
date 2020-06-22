96258311e8af178c712254d0b0e2878af79c0d66
unihubster <var@var>
2020-06-22 14:05:53 +0300

    model classes

---
+++ src/main/java/net/demo/chat/model/ChatOutMessage.java
@@ -0,0 +1,33 @@
+package net.demo.chat.model;
+
+import java.util.Date;
+
+public class ChatOutMessage {
+    private String content;
+    private String groupName;
+    private Date timestamp;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+}
