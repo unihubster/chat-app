49a5f6b30d1e9bec0c5c6a70c9f3247ca139893d
unihubster <var@var>
2020-06-22 14:31:32 +0300

    message controller

---
+++ src/main/java/net/demo/chat/controller/ChatController.java
@@ -0,0 +1,25 @@
+package net.demo.chat.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import net.demo.chat.model.ChatInMessage;
+import net.demo.chat.model.ChatOutMessage;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/guestchat")
+    @SendTo("/topic/guestchats")
+    public ChatOutMessage handleMessaging(ChatInMessage chatInMessage) throws Exception {
+        Thread.sleep(1000); // simulate delay of real server
+        return new ChatOutMessage(chatInMessage.getMessage());
+    }
+
+    @MessageMapping("/guestupdate")
+    @SendTo("/topic/guestupdates")
+    public ChatOutMessage handleUserIsTyping(ChatInMessage chatInMessage) throws Exception {
+        return new ChatOutMessage("Someone is typing...");
+    }
+}
